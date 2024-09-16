10550a0ecc4f5af8db763d5e173525a10f37de38
Kevin Declan <kdeclann@gmail.com>
2024-09-16 15:50:47 -0500

    ...

--- server.js
+++ server.js
@@ -2,28 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware for handling data parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json(), express.urlencoded({ extended: true }), cors());
 
-app.use(cors());
-
-// Connect to MongoDB and start server
 mongoose.connect(process.env.DB)
-    .then(() => {
-        console.log('MongoDB connected...');
-
-        // Start server after MongoDB is connected
-        app.listen(PORT, () => {
-            console.log(`Server running and connected to DB on port ${PORT}`);
-        });
-    })
-    .catch(err => console.log('MongoDB connection error:', err));
+  .then(() => {
+    console.log('MongoDB connected...');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.log('MongoDB connection error:', err));
