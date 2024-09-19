a93de7cd9ebf20d311ca10bf18e7267a1e21880c
Declan <kdeclann@gmail.com>
2024-09-19 12:12:40 -0500

    Initial commit after new pc - may have fixed server issues

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json(), express.urlencoded({ extended: true }), cors());
+
+mongoose.connect(process.env.DB)
+  .then(() => {
+    console.log('MongoDB connected...');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.log('MongoDB connection error:', err));
