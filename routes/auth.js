048ef98aefbaafabbad6881b1eb5ffa93b17b4a4
Declan <kdeclann@gmail.com>
2024-09-21 10:57:51 -0500

    server issued resolved

---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// here is where you will call the auth code that will route to the server for confirmation? shouldnt be too hard check Ben Awad vid again
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json(), express.urlencoded({ extended: true }), cors());
 
-mongoose.connect(process.env.DB)
+const authRoutes = require("./routes/auth");
+
+mongoose
+  .connect(process.env.DB)
   .then(() => {
-    console.log('MongoDB connected...');
+    console.log("MongoDB connected...");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.log('MongoDB connection error:', err));
+  .catch((err) => console.log("MongoDB connection error:", err));
